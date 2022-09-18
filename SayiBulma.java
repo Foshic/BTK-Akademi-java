ce729c0985f9113f45855fd7ae30b74c26f39d81
FikretErduran <fikret.erd@gmail.com>
2022-09-18 03:06:45 +0200

    Add files via upload

---
+++ SayiBulma.java
@@ -0,0 +1,21 @@
+
+public class SayiBulma {
+
+	public static void main(String[] args)
+	{    int aranacakSayi =9;
+	
+	
+		int [] sayilar=new int [] {1,2,5,7,9,0};
+		
+		     for(int sayi:sayilar) 
+		{
+			if(sayi==aranacakSayi) {
+				System.out.println("aranan sayi :"+" "+aranacakSayi);
+			}
+			
+			
+			
+		}
+	}
+
+}
